f600fa2b39427dfb41ec86438bdb304e31c5d98d
Pedro Nave <pedromarininave123@gmail.com>
2024-07-06 20:43:51 -0300

    Final Project

---
+++ Consumer.ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Consumer.ConsoleApp;
+using Vax;
+
+var services = new ServiceCollection();
+
+services.AddSingleton<IConsoleWriter, ConsoleWriter>();
+//services.AddSingleton<IIdGenerator, IdGenerator>();
+//services.AddTransient<IIdGenerator, IdGenerator>();
+
+//services.AddTransient<ConsoleWriter>();
+//services.AddSingleton(new IdGenerator(new ConsoleWriter()));
+services.AddSingleton(provider => new IdGenerator(provider.GetService<IConsoleWriter>()!));
+
+var serviceProvider = services.BuildServiceProvider();
+
+//var service = serviceProvider.GetService<IConsoleWriter>();
+var service1 = serviceProvider.GetService<IdGenerator>();
+var service2 = serviceProvider.GetService<IdGenerator>();
+
+service1.PrintId();
+service2.PrintId();
---
+++ Vax/ServiceCollection.cs
@@ -0,0 +1,102 @@
+﻿namespace Vax;
+
+public class ServiceCollection : List<ServiceDescriptor>
+{
+    public ServiceCollection AddService(ServiceDescriptor serviceDescriptor)
+    {
+        Add(serviceDescriptor);
+        return this;
+    }
+
+    public ServiceCollection AddSingleton<TService>()
+        where TService : class
+    {
+        return AddSingleton<TService, TService>();
+    }
+
+    public ServiceCollection AddSingleton<TService>(Func<ServiceProvider, TService> factory)
+        where TService : class
+    {
+        var serviceDescriptor = new ServiceDescriptor()
+        {
+            ServiceType = typeof(TService),
+            ImplementationType = typeof(TService),
+            ImplementationFactory = factory,
+            Lifetime = ServiceLifetime.Singleton
+        };
+
+        AddService(serviceDescriptor);
+        return this;
+    }
+
+    public ServiceCollection AddSingleton(object implementation)
+    {
+        var serviceType = implementation.GetType();
+        var serviceDescriptor = new ServiceDescriptor()
+        {
+            ServiceType = serviceType,
+            ImplementationType = serviceType,
+            Implementation = implementation,
+            Lifetime = ServiceLifetime.Singleton
+        };
+
+        AddService(serviceDescriptor);
+        return this;
+    }
+
+    public ServiceCollection AddTransient<TService>()
+        where TService : class
+    {
+        return AddTransient<TService, TService>();
+    }
+
+    public ServiceCollection AddTransient<TService>(Func<ServiceProvider, TService> factory)
+    where TService : class
+    {
+        var serviceDescriptor = new ServiceDescriptor()
+        {
+            ServiceType = typeof(TService),
+            ImplementationType = typeof(TService),
+            ImplementationFactory = factory,
+            Lifetime = ServiceLifetime.Transient
+        };
+
+        AddService(serviceDescriptor);
+        return this;
+    }
+
+    public ServiceCollection AddSingleton<TService, TImplementation>()
+        where TService : class
+        where TImplementation : class, TService
+    {
+        ServiceDescriptor serviceDescriptor = AddServiceDescriptorWithLifetime<TService, TImplementation>(ServiceLifetime.Singleton);
+
+        Add(serviceDescriptor);
+        return this;
+    }
+
+    public ServiceCollection AddTransient<TService, TImplementation>()
+        where TService : class
+        where TImplementation : class, TService
+    {
+        ServiceDescriptor serviceDescriptor = AddServiceDescriptorWithLifetime<TService, TImplementation>(ServiceLifetime.Transient);
+
+        Add(serviceDescriptor);
+        return this;
+    }
+
+    private static ServiceDescriptor AddServiceDescriptorWithLifetime<TService, TImplementation>(ServiceLifetime lifetime)
+    {
+        return new ServiceDescriptor()
+        {
+            ServiceType = typeof(TService),
+            ImplementationType = typeof(TImplementation),
+            Lifetime = lifetime
+        };
+    }
+
+    public ServiceProvider BuildServiceProvider()
+    {
+        return new ServiceProvider(this);
+    }
+}
